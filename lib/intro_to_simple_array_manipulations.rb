785f0e902819201343afadd91adf4362300f847a
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-28 03:29:38 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_push(Array, String)
+  Array.push(String)
+end
+
+def using_unshift(Array, String)
+  Array.unshift(String)
+end
+
+def shift_with_args(Array)
+  Array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end  
